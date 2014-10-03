673dc04eae80f6d8b855ff7bd3788485c326b2d2
Enrique Benavides <enriquebenavidesm@gmail.com>
2014-10-03 02:34:58 -0600

    ejemplo Mvc

---
+++ clientes/application/controllers/controller.php
@@ -0,0 +1,23 @@
+
+<?php
+	include_once Coex::registry("models").'model.php';
+	class Controller {
+	public $model;
+	public function __construct()
+	    {
+	        $this->model = new Model();
+	    }
+	public function invoke()
+	{
+	$reslt = $this->model->getlogin();     // it call the getlogin() function of model class and store the return value of this function into the reslt variable.
+	if($reslt == 'login')
+	{
+		include Coex::registry("views").'afterlogin.php';
+	}
+	else
+	{
+		include Coex::registry("views").'login.php';
+	}
+	}
+	}
+?>
