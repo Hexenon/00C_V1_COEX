119d97056196c351b4fadb73e3ffdedcb0097d14
Hexenon <enriquebenavidesm@gmail.com>
2014-10-04 10:25:05 -0600

    test

--- clientes/application/config/config.php
+++ clientes/application/config/config.php
@@ -38,7 +38,7 @@ define('DB_PASSWORD','eKcGZr59zAa2BEWU');
 /**
  *  Variable para identificar si es un test de desarrollo 
  */
-define('TEST_ENVIROMENT',true);
+define('TEST_ENVIROMENT',false);
 define('SECURE', true);
 define('LOG_FILE', 'system.log');
 define('DO_LOG', true);
